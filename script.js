33dd3f139f9d6ff3281aacebe525f53e3550ec1e
Priya Kushawaha <priyakushawaha73@gmail.com>
2024-04-30 20:03:02 +0545

    portfolio

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const socialIcons = document.querySelectorAll('.home-sci a');
+
+    socialIcons.forEach(icon => {
+        icon.addEventListener('click', (e) => {
+            e.preventDefault(); // Prevent default link behavior
+            const className = icon.querySelector('i').className;
+
+            switch (className) {
+                case 'bx bxl-facebook':
+                    window.open('https://www.facebook.com/singhpriya03', '_blank');
+                    break;
+                case 'bx bxl-github':
+                    window.open('https://github.com/Kushpriya', '_blank');
+                    break;
+                case 'bx bxl-linkedin':
+                    window.open('https://www.linkedin.com/in/kushpriya/', '_blank');
+                    break;
+                case 'bx bxl-twitter':
+                    window.open('https://twitter.com/kush_priya07', '_blank');
+                    break;
+                case 'bx bxl-youtube':
+                    window.open('https://www.youtube.com/@kushpriya07', '_blank');
+                    break;
+                default:
+                    break;
+            }
+        });
+    });
+});
